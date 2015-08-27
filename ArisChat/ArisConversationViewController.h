d6540b4d591b97ab325a22b4740e32763603dbc1
wuchen <wwfyc798@gmail.com>
2015-08-27 14:49:41 -0500

    update

---
+++ ArisChat/ArisConversationViewController.h
@@ -0,0 +1,32 @@
+//
+//  ArisConversationViewController.h
+//  ArisChat
+//
+//  Created by Wuchen Wang on 10/19/14.
+//  Copyright (c) 2014 Wuchen Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SendView.h"
+
+@interface ArisConversationViewController : UIViewController
+{
+    float prevLines;
+    UIButton *sendButton;
+    UIButton *sendButton2;
+    UIButton *circleButton;
+}
+@property (nonatomic,strong) NSString *cleanName;
+@property (nonatomic,strong) NSString *conversationJidString;
+@property (nonatomic,strong) UITableView *mtableView;
+@property (nonatomic,strong) NSMutableArray* chats;
+@property (nonatomic,strong) UILabel *statusLabel;
+@property (nonatomic,strong) SendView *sendView;
+@property (nonatomic,strong) UITextView *msgText;
+
+-(void)showConversationForJIDString:(NSString *)jidString;
+-(void)shortenTableView;
+-(void)scrollToBottomAnimated:(BOOL)animated;
+-(void)loadData;
+
+@end
